44f254291df6ec8bfe195f080e3b9a8efdffbdd9
erfanmoshafi <erfan.moshafi2018@gmail.com>
2022-01-28 19:45:03 +0330

    style

--- index.php
+++ index.php
@@ -14,25 +14,54 @@
             border-radius:5px;
         }
         .chat-box{
-            width: 700px;
+            width: 100%;
             height: 300px;
             border: 1px solid #000;
             border-radius:5px
         }
         .send-box{
-            width: 700px;
+            width: 100%;
             height: 50px;
         }
-        #message {
-            width: 100%
+        #message,.btn-send {
+            padding: 10px;
+            border-radius: 5px;
+            margin-top: 5px;
+            font-size: 20px;
+            float: left;
+        }
+        #message{
+            width: 80%;
+        }
+        .bot-pm , .user-pm{
+            border: 1px solid #000;
+            border-radius:5px;
+            min-height:10px;
+            height:auto;
+            width:300px;
+            padding:10px
+        }
+        .bot-pm{
+            float:left;
+        }
+        .user-pm{
+            float:right;
         }
     </style>
 </head>
 <body>
     <div class="container-chat">
-        <div class="chat-box"></div>
+        <div class="chat-box">
+            <div class="bot-pm">
+
+            </div>
+            <div class="user-pm">
+                
+           </div>
+        </div>
         <div class="send-box">
              <input type="text" id="message">
+            <button type="button" class="btn-send">send</button>
         </div>
 
     </div>
